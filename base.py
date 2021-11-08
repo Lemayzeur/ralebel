58642b315b2b64e9739e0f2a90f33c9837c7a17d
lemayzeur <lemayzeur@code9haiti.com>
2021-11-08 11:41:39 -0500

    Power method added

--- base.py
+++ base.py
@@ -10,6 +10,7 @@ TT_PLUS		= 'PLUS'
 TT_MINUS	= 'MIN'
 TT_MUL		= 'MUL'
 TT_DIV		= 'DIV'
+TT_POW		= 'POW'
 TT_LPAREN	= 'LPAREN'
 TT_RPAREN	= 'RPAREN'
 TT_EOF		= 'EOF'
@@ -137,6 +138,9 @@ class Lexer:
 				self.advance()
 			elif self.current_char  == '/':
 				tokens.append(Token(TT_DIV, pos_start=self.pos))
+				self.advance()
+			elif self.current_char  == '^':
+				tokens.append(Token(TT_POW, pos_start=self.pos))
 				self.advance()
 			elif self.current_char  == '(':
 				tokens.append(Token(TT_LPAREN, pos_start=self.pos))
@@ -266,17 +270,11 @@ class Parser:
 
 	###################################
 
-	def factor(self):
+	def atom(self):
 		res = ParseResult()
 		tok = self.current_tok
 
-		if tok.type in (TT_PLUS, TT_MINUS):
-			res.register(self.advance())
-			factor = res.register(self.factor())
-			if res.error: return res
-			return res.success(UnaryOpNode(tok, factor))
-		
-		elif tok.type in (TT_INT, TT_FLOAT):
+		if tok.type in (TT_INT, TT_FLOAT):
 			res.register(self.advance())
 			return res.success(NumberNode(tok))
 
@@ -295,8 +293,23 @@ class Parser:
 
 		return res.failure(InvalidSyntaxError(
 			tok.pos_start, tok.pos_end,
-			"Nou atann antye ou desimal"
+			"Nou atann antye | desimal"
 		))
+
+	def power(self):
+		return self.bin_op(self.atom, (TT_POW, ), self.factor)
+
+	def factor(self):
+		res = ParseResult()
+		tok = self.current_tok
+
+		if tok.type in (TT_PLUS, TT_MINUS):
+			res.register(self.advance())
+			factor = res.register(self.factor())
+			if res.error: return res
+			return res.success(UnaryOpNode(tok, factor))
+		
+		return self.power()
 
 	def term(self):
 		return self.bin_op(self.factor, (TT_MUL, TT_DIV))
@@ -306,15 +319,18 @@ class Parser:
 
 	###################################
 
-	def bin_op(self, func, ops):
+	def bin_op(self, func_a, ops, func_b=None):
+		if func_b == None:
+			func_b = func_a
+		
 		res = ParseResult()
-		left = res.register(func())
+		left = res.register(func_a())
 		if res.error: return res
 
 		while self.current_tok.type in ops:
 			op_tok = self.current_tok
 			res.register(self.advance())
-			right = res.register(func())
+			right = res.register(func_b())
 			if res.error: return res
 			left = BinOpNode(left, op_tok, right)
 
@@ -379,6 +395,10 @@ class Number:
 				)
 
 			return Number(self.value / other.value).set_context(self.context), None
+			
+	def powed_by(self, other):
+		if isinstance(other, Number):
+			return Number(self.value ** other.value).set_context(self.context), None
 
 	def __repr__(self):
 		return str(self.value)
@@ -427,6 +447,8 @@ class Interpreter:
 			result, error = left.multed_by(right)
 		elif node.op_tok.type == TT_DIV:
 			result, error = left.dived_by(right)
+		elif node.op_tok.type == TT_POW:
+			result, error = left.powed_by(right)
 
 		if error:
 			return res.failure(error)
