f3b500da71644df29682fa45707760b8a5122372
lemayzeur <lemayzeur@code9haiti.com>
2021-11-08 11:06:25 -0500

    run function

--- base.py
+++ base.py
@@ -448,3 +448,20 @@ class Interpreter:
 		else:
 			return res.success(number.set_pos(node.pos_start, node.pos_end))
 
+def run(fn, text):
+	# Generate tokens
+	lexer = Lexer(fn, text)
+	tokens, error = lexer.make_tokens()
+	if error: return None, error
+	
+	# Generate AST
+	parser = Parser(tokens)
+	ast = parser.parse()
+	if ast.error: return None, ast.error
+
+	# Run program
+	interpreter = Interpreter()
+	context = Context('<program>')
+	result = interpreter.visit(ast.node, context)
+
+	return result.value, result.error
